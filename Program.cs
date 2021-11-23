f398901663e2c1f4cd4cf61e3cc57dcd53fc8716
djinN999 <94804415+djinN999@users.noreply.github.com>
2021-11-23 11:03:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace part3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		string name, alter, tier, hobby;
+			
+			Console.WriteLine("Nenne deinen Namen:  ");
+			name = Console.ReadLine();
+			
+			Console.WriteLine("Nenne dein Alter:  ");
+			alter = Console.ReadLine();
+			
+			Console.WriteLine("Nenne dein Lieblingstier:  ");
+			tier = Console.ReadLine();
+			
+			Console.WriteLine("Nenne eins deiner Hobbys:  ");
+			hobby = Console.ReadLine();
+		
+			
+			Console.WriteLine("\n\nHallo {0}! Da du ja schon {1} bist, ", name , alter);
+			Console.WriteLine("gehe ich davon aus, dass du schon einiges ausprobiert hast!");
+			Console.WriteLine("WOW! {0}, welch ein interessantes Hobby du doch hast!", hobby);
+			Console.WriteLine("Mir war sofort bewusst, dass {0} dein Lieblingstier ist!", tier);
+			
+
+			Console.ReadKey(true);
+		}
+	}
+}
